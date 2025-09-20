13f5cbf3696ee93aef0f771b72fd2574f58250ca
vbalaji215-dev <vbalaji215@outlook.com>
2025-09-20 07:21:39 -0400

    Created master sql file

---
+++ db_scripts/postgres/centene/schema/pp_net_attrib.sql
@@ -0,0 +1,8 @@
+CREATE TABLE portown.pp_net_attrib (
+	id integer NOT NULL,
+	net_id integer NOT NULL,
+	attribute_id integer NOT NULL,
+	CONSTRAINT pp_net_attrib_pk PRIMARY KEY (id),
+	CONSTRAINT pp_net_attrib_fmg_attribute_types_fk FOREIGN KEY (attribute_id) REFERENCES portown.fmg_attribute_types(id),
+	CONSTRAINT pp_net_attrib_pp_net_fk FOREIGN KEY (net_id) REFERENCES portown.pp_net(id)
+);
---
+++ db_scripts/postgres/master.sql
@@ -0,0 +1,14 @@
+-- Run schema scripts
+\i schema/drop_tables.sql
+\i centene/schema/fmg_attribute_types.sql
+\i centene/schema/fmg_attribute_fields.sql
+\i centene/schema/pp_net.sql
+\i centene/schema/pp_net_attrib.sql
+\i centene/schema/pp_net_attrib_values.sql
+\i centene/schema/fmg_cities.sql
+\i centene/schema/fmg_counties.sql
+\i schema/create_tables.sql
+\i centene/data/fmg_attribute_types.sql
+\i centene/data/fmg_attribute_fields.sql
+\i centene/data/pp_net.sql
+\i data/insert_data.sql
