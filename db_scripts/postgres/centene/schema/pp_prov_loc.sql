55c83f95b4be1ec0616e08dcc5914ac074d6c747
vbalaji215-dev <78884455+vbalaji215-dev@users.noreply.github.com>
2025-09-20 20:51:15 -0400

    Update pp_prov_loc.sql

--- db_scripts/postgres/centene/schema/pp_prov_loc.sql
+++ db_scripts/postgres/centene/schema/pp_prov_loc.sql
@@ -2,10 +2,10 @@ CREATE TABLE portown.pp_prov_loc (
 	prov_id integer NOT NULL,
 	loc_id integer NOT NULL,
 	name_usage char(1) NULL,
-	"primary" char(1) NULL,
+	"PRIMARY" char(1) NULL,
 	start_date date NULL,
 	end_date date NULL,
-	print_supress char(1) NULL,
+	print_suppress char(1) NULL,
     label_cluster_id numeric NULL,
 	CONSTRAINT pp_prov_loc_pp_prov_fk FOREIGN KEY (prov_id) REFERENCES portown.pp_prov(id),
 	CONSTRAINT pp_prov_loc_pp_prov_tin_loc_fk FOREIGN KEY (loc_id) REFERENCES portown.pp_prov_tin_loc(id)
