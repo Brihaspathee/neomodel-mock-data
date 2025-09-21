bbab12d23e5213357b84995cf2bbde4c739735f8
vbalaji215-dev <78884455+vbalaji215-dev@users.noreply.github.com>
2025-09-20 20:51:34 -0400

    Update pp_prov_net_loc_cycle.sql

--- db_scripts/postgres/centene/schema/pp_prov_net_loc_cycle.sql
+++ db_scripts/postgres/centene/schema/pp_prov_net_loc_cycle.sql
@@ -5,7 +5,7 @@ CREATE TABLE portown.pp_prov_net_loc_cycle (
 	loc_id integer NOT NULL,
 	start_date date NOT NULL,
 	end_date date NOT NULL,
-	"primary" char(1) NOT NULL,
+	"PRIMARY" char(1) NOT NULL,
     label_cluster_id numeric NULL,
 	CONSTRAINT pp_prov_net_loc_cycle_pk PRIMARY KEY (id),
 	CONSTRAINT pp_prov_net_loc_cycle_pp_prov_net_cycle_fk FOREIGN KEY (prov_net_cycle_id) REFERENCES portown.pp_prov_net_cycle(id),
