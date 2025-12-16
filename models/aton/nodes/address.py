49c43cb718ad319dc41cf5a7e19bb58ac0a48336
vbalaji215-dev <vbalaji215@outlook.com>
2025-12-16 12:40:18 -0500

    New index cypher

--- models/aton/nodes/address.py
+++ models/aton/nodes/address.py
@@ -8,9 +8,9 @@ class Address(BaseNode):
     secondaryAddress = StringProperty(required=False)
     city = StringProperty(required=False)
     state = StringProperty(required=False)
-    zip_code = StringProperty(required=False)
+    zip_code = StringProperty(required=False, db_property="zipCode")
     county = StringProperty(required=False)
-    county_fips = StringProperty(required=False)
+    county_fips = StringProperty(required=False, db_property="countyFIPS")
     latitude = StringProperty(required=False)
     longitude = StringProperty(required=False)
 
